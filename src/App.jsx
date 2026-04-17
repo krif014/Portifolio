20f17ae039dfd5d53841d71acd16533df489286c
krif014 <krif014@gmail.com>
2026-04-17 10:00:00 +0200

    Adding the needed and essestial pictures

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
     return (
-      <div className="h-[1400px] text-7xl text-emerald-600">
+      <div className="min-h-screen bg-black">
         App
       </div>
 
