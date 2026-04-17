9c007b7b9cc0c75eca461564c1f6b60602152bd6
krif014 <krif014@gmail.com>
2026-04-17 10:00:00 +0200

    Setting skills.js

---
+++ src/data/skills.js
@@ -0,0 +1,51 @@
+export const skills = [
+    {
+        id: 1,
+        name: 'React.js',
+        icon: 'Code2',
+        level: 'Expert',
+        experience: '3+ years'
+    },
+    {
+        id: 2,
+        name: 'Next.js',
+        icon: 'Zap',
+        level: 'Advanced',
+        experience: '2+ years'
+    },
+    {
+        id: 3,
+        name: 'Node.js',
+        icon: 'Server',
+        level: 'Advanced',
+        experience: '2+ years'
+    },
+    {
+        id: 4,
+        name: 'JavaScript (ES6+)',
+        icon: 'Braces',
+        level: 'Expert',
+        experience: '4+ years'
+    },
+    {
+        id: 5,
+        name: 'Tailwind CSS',
+        icon: 'Palette',
+        level: 'Expert',
+        experience: '2+ years'
+    },
+    {
+        id: 6,
+        name: 'MongoDB',
+        icon: 'Database',
+        level: 'Intermediate',
+        experience: '2+ years'
+    },
+    {
+        id: 7,
+        name: 'Git & GitHub',
+        icon: 'GitBranch',
+        level: 'Advanced',
+        experience: '3+ years'
+    }
+]
