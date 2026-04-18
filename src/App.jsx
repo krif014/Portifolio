32b0fbdf50ab38dc812d9936d697c79815ddc169
krif014 <krif014@gmail.com>
2026-04-18 10:00:00 +0200

    Modifying the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import ContactSection from "./components/sections/ContactSection";
+import Hero from "./components/sections/Hero";
+import ProjectsSection from "./components/sections/ProjectsSection";
+import ServicesSection from "./components/sections/ServicesSection";
+import SkillsSection from "./components/sections/SkillsSection";
+import TechStack from "./components/sections/TechStack";
+import TestimonialsSection from "./components/sections/TestimonialsSection";
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
 
-const App = () => {
-    return (
-      <div className="min-h-screen bg-black">
-        App
-      </div>
-
-    )
-}   
-
-export default App
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black text-white antialiased">
+      <Navbar />
+      <main>
+        <Hero />
+        <TechStack />
+        <SkillsSection />
+        <ProjectsSection />
+        <ServicesSection />
+        <TestimonialsSection />
+        <ContactSection />
+      </main>
+      <Footer />
+    </div>
+  );
+}
