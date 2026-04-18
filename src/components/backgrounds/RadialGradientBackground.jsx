7255f1ca2a80329806b27bc01274923fdc432152
krif014 <krif014@gmail.com>
2026-04-18 10:00:00 +0200

    Modifying the RadialGradient

--- src/components/backgrounds/RadialGradientBackground.jsx
+++ src/components/backgrounds/RadialGradientBackground.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+export default function RadialGradientBackground({
+  className = "",
+  position = "center",
+}) {
+  const posClass =
+    position === "right"
+      ? "right-0 top-1/4 translate-x-1/3"
+      : position === "left"
+        ? "left-0 top-1/2 -translate-x-1/3"
+        : "left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2";
 
-const RadialGradientBackground = () => {
   return (
-    <div>
-      RadialGradientBackground
-    </div>
-  )
+    <div
+      className={`pointer-events-none absolute ${posClass} h-[min(90vw,640px)] w-[min(90vw,640px)] rounded-full bg-accent/20 blur-[100px] opacity-35 ${className}`}
+      aria-hidden
+    />
+  );
 }
-
-export default RadialGradientBackground
